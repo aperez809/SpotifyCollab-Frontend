33770776e4e12870aef75a554a7fc16165aff3d8
welchdaniel <welch.da@husky.neu.edu>
2019-06-03 18:58:17 -0400

    created spotify-login component

---
+++ src/app/spotify-login/spotify-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spotify-login',
+  templateUrl: './spotify-login.component.html',
+  styleUrls: ['./spotify-login.component.css']
+})
+export class SpotifyLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
