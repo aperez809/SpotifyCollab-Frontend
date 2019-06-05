1eb3ea0034b1ac07a8044a613d12dc07d9120959
welchdaniel <welch.da@husky.neu.edu>
2019-06-04 23:39:41 -0400

    fixed profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SpotifyServiceClient } from './services/spotify-service-client';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
