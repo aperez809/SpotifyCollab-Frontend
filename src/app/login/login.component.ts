c3665428430a12d3de18c04fd2d654f111159cd6
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-06-20 18:34:18 -0400

    checkpointing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,15 +10,17 @@ import { UserService } from '../services/user-service-client';
 export class LoginComponent implements OnInit {
   username: String;
   password: String;
+  failedLogin: Boolean = false;
 
   constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {}
 
   login() {
-    const user = this.userService.assignSessionToUser(this.username, this.password);
-    if (user != null) {
-      this.router.navigate(['profile', user["_id"]]);
+    const userId = this.userService.assignSessionToUser(this.username, this.password);
+    if (userId) {
+      console.log(userId);
+      this.router.navigate(['profile', userId]);
     }
   }
 
--- src/app/services/user-service-client.ts
+++ src/app/services/user-service-client.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 
 @Injectable()
 export class UserService { 
@@ -34,32 +35,32 @@ export class UserService {
 
       }
           
-      assignSessionToUser(un, pw) { 
-        this.findUserByCredentials(un, pw). then(user => {
-        console.log(user);
-        if (user) {
-        const assignedSessionId = user["_id"];
-        const requestSessionUrl = "https://song-request-server-node.herokuapp.com/api/session/set/:name/:value".replace(":name", "sessionId").replace(":value", assignedSessionId)
-        console.log(requestSessionUrl);
-        return fetch(requestSessionUrl, {
-          credentials: 'include',
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json'
+      assignSessionToUser(un: String, pw: String) { 
+        return this.findUserByCredentials(un, pw).then(user => {
+          if (user) {
+            const assignedSessionId = user["_id"];
+            const requestSessionUrl = "https://song-request-server-node.herokuapp.com/api/session/set/:name/:value".replace(":name", "sessionId").replace(":value", assignedSessionId);
+            return fetch(requestSessionUrl, {
+              credentials: 'include',
+              method: 'POST',
+              headers: {
+                'content-type': 'application/json'
+              }
+            })
+            .then(res => {
+              //console.log(res.json())
+              return res.json();
+            })
+            .then(resData => {
+              return resData;
+            });
           }
-        })
-        .then(res => {
-          try {
-            return res.json()
+          else {
+            console.log("cunt");
+            return;
           }
-          catch(error) {
-            
-          }
-        });
-      }
 
-      else {
-      }
+          
       })
-    }     
+       }     
 }
