8d3b351e38f2f65b10bc9c649dd7b86c2f3342ab
haibero <owen.haiber@gmail.com>
2019-06-20 20:33:39 -0400

    register skeleton

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user-service-client';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  username: String;
+  password: String;
+  firstName: String;
+  lastName: String;
+  registerFailed: false;
+
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  register() {
+    this.userService.createUser(this.username, this.password, this.firstName, this.lastName)
+      .then(user => {
+        if (user) {
+          this.userService.assignSessionToUser(user);
+          this.router.navigate(['login', user["_id"]]);
+        }
+      });
+    /*if (userId) {
+      this.router.navigate(['profile', userId]);
+    }
+  }*/
   }
 
 }
