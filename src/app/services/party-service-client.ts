337dcc37655f5a2fad3044c6b95d730faff122e4
haibero <owen.haiber@gmail.com>
2019-06-22 21:42:03 -0400

    Create party works as intended

--- src/app/services/party-service-client.ts
+++ src/app/services/party-service-client.ts
@@ -1,11 +1,56 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import {UrlSerializer} from "@angular/router";
+import {UserService} from "./user-service-client";
 
 @Injectable()
 export class PartyService {
 
 
-  constructor(private cookieService: CookieService) {  }
+  constructor(private cookieService: CookieService,
+              private userService: UserService) {  }
+
+
+
+  getPartyList() {
+    return fetch(    "http://song-request-server-node.herokuapp.com/api/parties", {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(res => {
+      return res.json();
+    });
+  }
+
+  createParty = async(partyName, ownerId) => {
+    const owner = await this.userService.findUserById(ownerId);
+    return fetch("http://song-request-server-node.herokuapp.com/api/parties", {
+      credentials: 'include',
+      method: 'POST',
+      body: JSON.stringify({
+        "partyName": partyName,
+        "attendees": [owner],
+        partyLeader: owner,
+        queue: [],
+        "passwordReq": false,
+        "password": null,
+        bannedUsers: [],
+      }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.cookieService.set("currentPartyId", res["_id"], undefined, "/");
+        return this.userService.updateUserParty(ownerId, res["_id"])})
+      .then(res => {
+        return this.userService.updateUserType(ownerId, 'DJ');
+    }).then(res => {
+        return this.setDJ(this.cookieService.get("currentPartyId"), ownerId);
+    });
+  }
 
 
   async findPartyById(partyId: String) {
@@ -57,7 +102,7 @@ export class PartyService {
   }
 
   setDJ = (pid, userId) => {
-    return fetch("http://song-request-server-node.herokuapp.com/api/parties" + pid + "/setDJ/" + userId, {
+    return fetch("http://song-request-server-node.herokuapp.com/api/parties/" + pid + "/setDJ/" + userId, {
       method: 'PUT',
       headers: {
         'content-type': 'application/json'
